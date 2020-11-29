a0367a99f7f56049c3f0151912fb179b143d2741
kabolov <alexander.kabolov@gmail.com>
2020-11-29 23:01:21 +0300

    auth lambda setuped

---
+++ authorization-service/src/basicAuthorizer.js
@@ -0,0 +1,39 @@
+const basicAuthorizer = (event, ctx, callback) => {
+  console.log("Event:" + event);
+
+  if (event["type"] != "TOKEN") callback("Unauthorized");
+
+  try {
+    const { authorizationToken } = event;
+
+    const encodedCreds = authorizationToken.split(" ")[1];
+    const buffer = Buffer.from(encodedCreds, "base64");
+    const [username, password] = buffer.toString("utf-8").split(":");
+
+    const storedUserPassword = process.env[username];
+    const effect =
+      !storedUserPassword || storedUserPassword != password ? "Deny" : "Allow";
+
+    const policy = generatePolicy(encodedCreds, event.methodArn, effect);
+
+    callback(null, policy);
+  } catch (error) {
+    callback(`Unauthorized: ${error.message}`);
+  }
+};
+
+const generatePolicy = (principalId, resource, effect = "Allow") => ({
+  principalId,
+  policyDocument: {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: "execute-api:Invoke",
+        Effect: effect,
+        Resource: resource,
+      },
+    ],
+  },
+});
+
+module.exports = { basicAuthorizer };
